1d46caf329730fd1bd4551f1fc1a2876136dbc98
Indro <indro3311@gmail.com>
2026-05-21 20:01:26 +0600

    Prb3

---
+++ two_sum.cpp
@@ -0,0 +1,29 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+};
+int main(){
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = sol.twoSum(nums, target);
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No two sum solution found." << endl;
+    }
+    return 0;
+}
